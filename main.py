4d1fbc72136efdc4d1205ad3e2441d14e667f779
4rutemu <artyom.denega@gmail.com>
2022-03-13 15:27:28 +0300

    Создание первого спрайта игрока

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 import random
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+
 WIDTH = 360
 HEIGHT = 480
 FPS = 30
@@ -16,6 +25,9 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Цикл игры
 running = True
 while running:
@@ -26,6 +38,12 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+    # Обновление
+    all_sprites.update()
+
+    # Отрисовка
     screen.fill(BLACK)
+    all_sprites.draw(screen)
+
     pygame.display.flip()
 pygame.quit()
