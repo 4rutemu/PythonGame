0bafcdc03ab35fbd1ffb64ba65607c4bd57f1ddf
Serge <92951189+IoriKimura@users.noreply.github.com>
2022-04-06 14:52:05 +0300

    Главное меню
    Сделано главное меню с запуском игры
    Отдельный класс под кнопки, который потом можно использовать под любые кнопочки
    Картиночка для кнопочки Старта

---
+++ button.py
@@ -0,0 +1,24 @@
+import pygame
+class Button:
+    def __init__(self, x, y, image):
+        self.clicked = False
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self, surface):
+        action = False
+        # Узнаём позицию курсора
+        pos = pygame.mouse.get_pos()
+
+        # Находим курсор на кнопке и обработка кликов
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
