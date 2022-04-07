e0ac8d3610e3340439fcc6a67ab93c7dc86fd0f8
Serge <sergeykonstwork@gmail.com>
2022-04-08 00:29:51 +0300

    Enemy speed
    Враги с самого начала могут двигаться в разных направлениях, а не в одном; скорости разные для большей сложности

--- enemy.py
+++ enemy.py
@@ -6,37 +6,36 @@ import game_object
 import platform
 
 enemies = []
+speed_list = [-7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7]
 
 
 # TODO: получение урона от атак
 class Enemy(game_object.GameObject):
     def __init__(self, x, y):
         super().__init__(x, y, 30, 30, (255, 255, 0))
-        self.dx = 0
+        self.dx = speed_list[random.randint(0, 13)]
         # Так как враг будет перемещаться к игроку + для колизии
-        if random.random() != 0:
-            self.dx = 2
-        else:
-            self.dx = -2
 
     def enemy_collide(self, dx, pf, stop_list):
-        for p in pf:
+        for p, s in pf, stop_list:
             if sprite.collide_rect(self, p):  # Проверяем на пересечение противника с платформой
                 if dx > 0:  # Если противник движется вправо, то запрещаем
                     self.rect.right = p.rect.left
-                    self.dx = -2
+                    self.rect.right = s.rect.left
+                    self.dx *= -1
                 if dx < 0:  # Если противник движется в лево, то запрещаем
                     self.rect.left = p.rect.right
-                    self.dx = 2
+                    self.rect.left = s.rect.right
+                    self.dx *= -1
 
         for s in stop_list:
             if sprite.collide_rect(self, s):
                 if dx > 0:  # Если противник движется вправо, то запрещаем
                     self.rect.right = s.rect.left
-                    self.dx = -2
+                    self.dx *= -1
                 if dx < 0:  # Если противник движется в лево, то запрещаем
                     self.rect.left = s.rect.right
-                    self.dx = 2
+                    self.dx *= -1
 
     def moving(self, pf, stop_list):  # Функция с перемещением противника
         self.rect.x += self.dx
