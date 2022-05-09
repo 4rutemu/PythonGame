66f006fb6369ed474e4d8d7851d0322d3c67bc93
4rutemu <artyom.denega@gmail.com>
2022-05-09 12:36:18 +0300

    Добавление некоторых анимаций, небольшой рефакторинг кода
    добавлены анимации бездействия, бега вправо, прыжка и атаки вправо, левых собратьев добавлю позже

--- player.py
+++ player.py
@@ -11,8 +11,8 @@ import AttackSprite
 
 class Player(game_object.GameObject):
     def __init__(self):
-        super().__init__(x=parameters.WIDTH / 2, y=parameters.HEIGHT / 2, width=20,
-                         height=30, colour=parameters.GREEN)
+        super().__init__(x=parameters.WIDTH / 2, y=parameters.HEIGHT / 2, width=50,
+                         height=37, colour=parameters.GREEN)
 
         self.kill_score = 0
         self.hp = parameters.based_hp
@@ -25,14 +25,52 @@ class Player(game_object.GameObject):
         self.looking_right = True
         self.is_attacking = False
 
+        self.idle_images = [1, pygame.image.load("AnimationImages/adventurer-idle-00.png").convert_alpha(),
+                            pygame.image.load("AnimationImages/adventurer-idle-01.png").convert_alpha(),
+                            pygame.image.load("AnimationImages/adventurer-idle-00.png").convert_alpha()]
+
+        self.run_images = [1, pygame.image.load("AnimationImages/adventurer-run-00.png").convert_alpha(),
+                           pygame.image.load("AnimationImages/adventurer-run-01.png").convert_alpha(),
+                           pygame.image.load("AnimationImages/adventurer-run-02.png").convert_alpha(),
+                           pygame.image.load("AnimationImages/adventurer-run-03.png").convert_alpha(),
+                           pygame.image.load("AnimationImages/adventurer-run-04.png").convert_alpha()]
+
+        #self.jump_images = [1, pygame.image.load("AnimationImages/adventurer-jump-00.png").convert_alpha(),
+                            #pygame.image.load("AnimationImages/adventurer-jump-01.png").convert_alpha(),
+                            #pygame.image.load("AnimationImages/adventurer-jump-02.png").convert_alpha(),
+                            #pygame.image.load("AnimationImages/adventurer-jump-03.png").convert_alpha()]
+
+        self.jump_image = pygame.image.load("AnimationImages/adventurer-jump-03.png").convert_alpha()
+
+        self.attack_images = [1, pygame.image.load("AnimationImages/adventurer-attack2-00.png").convert_alpha(),
+                              pygame.image.load("AnimationImages/adventurer-attack2-01.png").convert_alpha(),
+                              pygame.image.load("AnimationImages/adventurer-attack2-02.png").convert_alpha(),
+                              pygame.image.load("AnimationImages/adventurer-attack2-03.png").convert_alpha(),
+                              pygame.image.load("AnimationImages/adventurer-attack2-04.png").convert_alpha(),
+                              pygame.image.load("AnimationImages/adventurer-attack2-05.png").convert_alpha()]
+
+        self.attack_image = pygame.image.load("AnimationImages/adventurer-attack2-03.png").convert_alpha()
+
+    def animation(self, animation_list, length):
+        if animation_list[0] >= length:
+            animation_list[0] = 1
+        self.image = animation_list[animation_list[0]]
+        animation_list[0] += 1
+
+#TODO: Анимашки!!! Порезать картинки!!!
+
     def get_input(self, pf, enemies):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            if self.onGround:
-                self.dy = -self.j_power
+        if keys[pygame.K_w] and self.onGround:
+            self.dy = -self.j_power
+            #self.animation(self.jump_images, len(self.jump_images))
+            self.image = self.jump_image
         if keys[pygame.K_d]:
             self.dx = self.move_speed
             self.looking_right = True
+            if self.onGround:
+                self.animation(self.run_images, len(self.run_images))
+
         if keys[pygame.K_a]:
             self.dx = -self.move_speed
             self.looking_right = False
@@ -44,7 +82,10 @@ class Player(game_object.GameObject):
             parameters.player_attack_sound.play(maxtime=1000)
             attack = AttackSprite.AttackSprite(owner='player', player=self)
             parameters.all_sprites.add(attack)
+            self.image = self.attack_image
             self.is_attacking = True
+        if not (keys[pygame.K_d] or keys[pygame.K_a] or keys[pygame.K_SPACE] or keys[pygame.K_w]) and self.onGround:
+            self.animation(self.idle_images, len(self.idle_images))
 
         self.onGround = False  # Неизвестно, когда он на земле
         self.rect.y += self.dy
@@ -85,5 +126,4 @@ class Player(game_object.GameObject):
 
     def update(self):
         self.get_input(pf=platform.platforms, enemies=enemy.enemies)
-
 
